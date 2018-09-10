9eb825190a8ebaf2d84e80d5e5c88bceca0daa4d
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-10 07:09:35 -0500

    commit

--- Poker_hand_comparison.rb
+++ Poker_hand_comparison.rb
@@ -1,7 +1,19 @@
 class PokerHand
+    
     def compare(hand)
-        hand.split(' ')
+        value = {
+        T: 10,
+        J: 11,
+        Q: 12,
+        K: 13,
+        A: 14
+    }
+        arr = hand.split(' ').map { |e| e.split('')[0] }
+        arr2 = []
+        for i in 0...arr.length
+            arr2 << value[:"#{arr[i]}"].to_i
+        end 
     end 
 end
-
-"KD JD AD 4D 7D"
+game1 = PokerHand.new
+p game1.compare("KD JD AD TD KD")
